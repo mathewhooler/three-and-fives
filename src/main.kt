cd345770bfedc5bd170cd724e290c7fda1f6d711
Mathew Hooler <mhooler@squareup.com>
2024-08-30 16:10:57 +1000

    changes made with Tina

--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,13 @@
-fun main() {
+fun main(){
   var sum = 0
   for (i in 0..999) {
-    if (i % 3 == 0 && i % 5 == 0){
+    if (i % 3 == 0 || i % 5 == 0){
       sum += i
     }
   }
   println("Total is $sum")
 }
+
+// fun sum3or5(): Int{
+//
+// }
