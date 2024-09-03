c0e0829e76d23555a0cef65aa490de7946128578
Mathew Hooler <mhooler@squareup.com>
2024-09-03 15:27:35 +1000

    added assertion to test

--- src/main/kotlin/Sum.kt
+++ src/main/kotlin/Sum.kt
@@ -1,7 +1,7 @@
 class Sum {
   fun calculate(): Int {
     var sum = 0
-    for (i in 0..999) {
+    for (i in 0..9) {
       if (i % 3 == 0 || i % 5 == 0) {
         sum += i
       }
--- src/test/kotlin/CalculateTest.kt
+++ src/test/kotlin/CalculateTest.kt
@@ -1,3 +1,5 @@
+
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class SumTest {
@@ -6,6 +8,7 @@ class SumTest {
   fun testCalculate() {
     val sum = Sum()
     val result = sum.calculate()
+    assertEquals(23, result, "The calculate function should add multiples of 3 or 5 for anything under the number 10")
   }
 }
 
