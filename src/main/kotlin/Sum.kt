1d35ad737a8eeeb82cba6e346ad17eb3578c393c
Mathew Hooler <mhooler@squareup.com>
2024-09-03 14:12:11 +1000

    fixed test

--- src/main/kotlin/Sum.kt
+++ src/main/kotlin/Sum.kt
@@ -1,9 +1,11 @@
 class Sum {
-  fun calculate(){
+  fun calculate(): Int {
     var sum = 0
-    for (i in 0..999){
-      if(i % 3 == 0 || i % 5 == 0)
+    for (i in 0..999) {
+      if (i % 3 == 0 || i % 5 == 0) {
         sum += i
+      }
     }
+    return sum
   }
 }
